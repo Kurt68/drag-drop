0ada80425180915bb5eb05a4780ed23346ae56cf
Kurt Hogentogler <kurt.ah@outlook.com>
2023-10-13 09:13:51 -0600

    Persist List Reordering using onDragEnd Callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,67 @@
 import initialData from './initial-data.js'
 import { Column } from './Column.jsx'
 import { DragDropContext } from '@hello-pangea/dnd'
+import { useState } from 'react'
 
 function App() {
-  const state = initialData
+  const [state, setState] = useState(initialData)
 
-  // onDragEnd = (result) => {
-  //   // TODO: reorder our column
-  // }
+  let onDragEnd = {}
+
+  // result object
+  // const result = {
+  //   draggableId: 'task-1',
+  //   type: 'TYPE',
+  //   reason: 'DROP',
+  //   source: {
+  //     droppableId: 'column-1',
+  //     index: 0,
+  //   },
+  //   destination: {
+  //     droppableId: 'column-1',
+  //     index: 1,
+  //   }
+  //   }
+
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result
+
+    if (!destination) {
+      return
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const column = state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    console.log(newTaskIds)
+
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+    console.log(newColumn)
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      
+      },
+    }
+    setState(newState)
+  }
 
   return (
-    <DragDropContext /*onDragEnd={onDragEnd}*/>
+    <DragDropContext onDragEnd={onDragEnd}>
       {state.columnOrder.map((columnId) => {
         const column = state.columns[columnId]
         const tasks = column.taskIds.map((taskId) => state.tasks[taskId])
