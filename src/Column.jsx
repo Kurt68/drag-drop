26de9df5e106d9aff22cf349112d98d803fe20a7
Kurt Hogentogler <kurt.ah@outlook.com>
2023-10-13 12:05:22 -0600

    Customized the Appearance of App during a Drag using snapshot values

--- src/Column.jsx
+++ src/Column.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { Task } from './Task'
-import { Droppable } from '@hello-pangea/dnd';
+import { Droppable } from '@hello-pangea/dnd'
 
 const Container = styled.div`
   margin: 8px;
@@ -12,20 +12,37 @@ const Title = styled.h3`
 `
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.5s ease;
+  background-color: ${(props) => (props.$isDraggingOver ? '#FBECB2' : 'white')};
 `
+
+// snapshot object has properties to style component on drag
+// Draggable
+// const draggableSnapshot = {
+//   isDragging: true,
+//   draggingOver: 'column-1',
+// }
+// Droppable
+// const droppableSnaphot = {
+//   isDraggingOver: true,
+//   draggingOverWith: 'task-1',
+// }
 
 export function Column({ column, tasks }) {
   return (
     <Container>
       <Title>{column.title}</Title>
       <Droppable droppableId={column.id}>
-        {(provided) => (
-          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <TaskList
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            $isDraggingOver={snapshot.isDraggingOver}
+          >
             {tasks.map((task, index) => (
               <Task key={task.id} task={task} index={index} />
             ))}
             {provided.placeholder}
-            
           </TaskList>
         )}
       </Droppable>
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,22 +1,34 @@
 import styled from 'styled-components'
-import { Draggable } from '@hello-pangea/dnd';
+import { Draggable } from '@hello-pangea/dnd'
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  background-color: ${(props) => (props.$isDragging ? '#072541' : 'white')};
+  color: ${(props) => (props.$isDragging ? 'white' : '#072541')};
 `
-
+// snapshot object has properties to style component on drag
+// Draggable
+// const draggableSnapshot = {
+//   isDragging: true,
+//   draggingOver: 'column-1',
+// }
+// Droppable
+// const droppableSnaphot = {
+//   isDraggingOver: true,
+//   draggingOverWith: 'task-1',
+// }
 export function Task({ task, index }) {
   return (
     <Draggable draggableId={task.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Container
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          $isDragging={snapshot.isDragging}
         >
           {task.content}
         </Container>
