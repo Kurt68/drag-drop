74d806974494acb815633cd32a456c8b3a99b761
Kurt Hogentogler <kurt.ah@outlook.com>
2023-10-16 12:04:14 -0600

    Need to add React.memo for performance gains to InnerLIst function and the container column

--- src/Column.jsx
+++ src/Column.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { Task } from './Task'
 import { Droppable, Draggable } from '@hello-pangea/dnd'
+import React from 'react'
 
 const Container = styled.div`
   margin: 8px;
@@ -20,7 +21,8 @@ const Title = styled.h3`
 const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.5s ease;
-  background-color: ${(props) => (props.$isDraggingOver ? '#FBECB2' : 'inherit')};
+  background-color: ${(props) =>
+    props.$isDraggingOver ? 'lightgrey' : 'inherit'};
   flex-grow: 1;
   min-height: 100px;
 `
@@ -36,33 +38,37 @@ const TaskList = styled.div`
 //   draggingOverWith: 'task-1',
 // }
 
+// Function could be more performat with React.memo
+// As it was broken out to be the function InnnerList
+function InnerList({ tasks }) {
+  return tasks.map((task, index) => (
+    <Task key={task.id} task={task} index={index} />
+  ))
+}
+
 export function Column({ column, tasks, index }) {
   return (
     <Draggable draggableId={column.id} index={index}>
-    {(provided) => (
-    <Container
-    {...provided.draggableProps}
-    ref={provided.innerRef}
-    >
-      <Title {...provided.dragHandleProps}>{column.title}</Title>
-      <Droppable droppableId={column.id} type=''>
-        {(provided, snapshot) => (
-          <TaskList
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            $isDraggingOver={snapshot.isDraggingOver}
-          >
-            {tasks.map((task, index) => (
+      {(provided) => (
+        <Container {...provided.draggableProps} ref={provided.innerRef}>
+          <Title {...provided.dragHandleProps}>{column.title}</Title>
+          <Droppable droppableId={column.id} type="">
+            {(provided, snapshot) => (
+              <TaskList
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                $isDraggingOver={snapshot.isDraggingOver}
+              >
+                {/* {tasks.map((task, index) => (
               <Task key={task.id} task={task} index={index} />
-            ))}
-            {provided.placeholder}
-          </TaskList>
-        )}
-      </Droppable>
-    </Container>
-
-    )}
+            ))} */}
+                <InnerList tasks={tasks} />
+                {provided.placeholder}
+              </TaskList>
+            )}
+          </Droppable>
+        </Container>
+      )}
     </Draggable>
-
   )
 }
